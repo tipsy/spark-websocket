838da4b4f804e22c86993bc7b3c4c7fb76df212d
David <davidaase@hotmail.com>
2015-09-20 19:10:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# spark-websocket
+The source code for the WebSocket chat application at sparktutorials.github.io: [https://sparktutorials.github.io/2015/11/08/spark-websocket-chat.html](https://sparktutorials.github.io/2015/11/08/spark-websocket-chat.html)
---
+++ src/main/java/Chat.java
@@ -0,0 +1,42 @@
+import org.eclipse.jetty.websocket.api.*;
+import org.json.*;
+import java.text.*;
+import java.util.*;
+import static j2html.TagCreator.*;
+import static spark.Spark.*;
+
+public class Chat {
+
+    static Map<Session, String> userUsernameMap = new HashMap<>();
+    static int nextUserNumber = 1; //Assign to username for next connecting user
+
+    public static void main(String[] args) {
+        staticFileLocation("public"); //index.html is served at localhost:4567 (default port)
+        webSocket("/chat", ChatWebSocketHandler.class);
+        init();
+    }
+
+    //Sends a message from one user to all users, along with a list of current usernames
+    public static void broadcastMessage(String sender, String message) {
+        userUsernameMap.keySet().stream().filter(Session::isOpen).forEach(session -> {
+            try {
+                session.getRemote().sendString(String.valueOf(new JSONObject()
+                    .put("userMessage", createHtmlMessageFromSender(sender, message))
+                    .put("userlist", userUsernameMap.values())
+                ));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    //Builds a HTML element with a sender-name, a message, and a timestamp,
+    private static String createHtmlMessageFromSender(String sender, String message) {
+        return article().with(
+                b(sender + " says:"),
+                p(message),
+                span().withClass("timestamp").withText(new SimpleDateFormat("HH:mm:ss").format(new Date()))
+        ).render();
+    }
+
+}
