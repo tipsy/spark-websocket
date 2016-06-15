2804437386eb7a37df5574aaf0c3c6a348a7d089
tipsy <davidaase@hotmail.com>
2016-06-15 20:28:29 +0200

    Spark 2.3 -> 2.5

--- src/main/java/Chat.java
+++ src/main/java/Chat.java
@@ -11,7 +11,8 @@ public class Chat {
     static int nextUserNumber = 1; //Assign to username for next connecting user
 
     public static void main(String[] args) {
-        staticFileLocation("/public"); //index.html is served at localhost:4567 (default port)
+        staticFiles.location("/public"); //index.html is served at localhost:4567 (default port)
+        staticFiles.expireTime(600);
         webSocket("/chat", ChatWebSocketHandler.class);
         init();
     }
