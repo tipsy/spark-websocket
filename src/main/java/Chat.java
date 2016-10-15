fcedcb12ef1d2786ad3596b3d01e3f845558e48d
David <davidaase@hotmail.com>
2016-10-15 20:44:55 +0200

    Make map thread-safe

--- src/main/java/Chat.java
+++ src/main/java/Chat.java
@@ -2,12 +2,15 @@ import org.eclipse.jetty.websocket.api.*;
 import org.json.*;
 import java.text.*;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
 import static j2html.TagCreator.*;
 import static spark.Spark.*;
 
 public class Chat {
 
-    static Map<Session, String> userUsernameMap = new HashMap<>();
+    // this map is shared between sessions and threads, so it needs to be thread-safe (http://stackoverflow.com/a/2688817)
+    static Map<Session, String> userUsernameMap = new ConcurrentHashMap<>();
     static int nextUserNumber = 1; //Assign to username for next connecting user
 
     public static void main(String[] args) {
